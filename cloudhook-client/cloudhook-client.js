359e80dfd6852e390dd852a4f7a91f07335bfdb6
Dan Galbraith <dan.galbraith.7@gmail.com>
2022-07-30 16:05:25 +0100

    Sorted

--- cloudhook-client/cloudhook-client.js
+++ cloudhook-client/cloudhook-client.js
@@ -5,22 +5,21 @@ module.exports = function(RED) {
         return require('socket.io-client')(uri, options);
     }
 
-    /* socket config */
+    /* Config */
     function CloudhookConfig(config) {
         RED.nodes.createNode(this, config);
         this.uri = config.uri;
-        this.options = config.options;
         //this.socket = connect(config.uri, JSON.parse(config.options || '{}'));
     }
     RED.nodes.registerType('cloudhook-config', CloudhookConfig);
    
-    /* sckt listener*/
+    /* Standard */
     function CloudhookStandard(config) {
         RED.nodes.createNode(this, config);
         const node = this;
         
         const server = RED.nodes.getNode(config.server);
-        const socket = connect(config.uri, {});
+        const socket = connect(server.uri, {});
         const hook = config.hooktenant + '/' + config.hookvalue;
 
         socket.on('connect', function() {
@@ -36,8 +35,12 @@ module.exports = function(RED) {
             node.status({ fill: 'red', shape: 'ring', text: 'error' });
         });
 
-        socket.on(hook, function (data) {
-            node.send({ topic: hook, payload: data });
+        socket.on("ch-get", function (data) {
+            node.send({ topic: hook, payload: data, method: "GET"});
+        })
+
+        socket.on("ch-post", function (data) {
+            node.send({ topic: hook, payload: data, method: "POST" });
         })
 
         node.on('close', function (done) {
@@ -47,6 +50,84 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType('cloudhook-standard', CloudhookStandard);
+
+
+    /* Load Balance */
+    function CloudhookLoadBalance(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+        
+        const server = RED.nodes.getNode(config.server);
+        const socket = connect(server.uri, {});
+        const hook = config.hooktenant + '/' + config.hookvalue;
+
+        socket.on('connect', function() {
+            socket.emit("ch-subscribe-lb", hook);
+            node.status({ fill: 'green', shape: 'dot', text: 'connected' });
+        });
+
+        socket.on('disconnect', function () {
+            node.status({ fill: 'red', shape: 'ring', text: 'disconnected' });
+        });
+
+        socket.on('connect_error', function(err) {
+            node.status({ fill: 'red', shape: 'ring', text: 'error' });
+        });
+
+        socket.on("ch-get", function (data) {
+            node.send({ topic: hook, payload: data, method: "GET"});
+        })
+
+        socket.on("ch-post", function (data) {
+            node.send({ topic: hook, payload: data, method: "POST" });
+        })
+
+        node.on('close', function (done) {
+            socket.disconnect();
+            node.status({});
+            done();
+        });
+    }
+    RED.nodes.registerType('cloudhook-loadbalance', CloudhookLoadBalance);
+
+
+    /* Hash */
+    function CloudhookHash(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+        
+        const server = RED.nodes.getNode(config.server);
+        const socket = connect(server.uri, {});
+        const hook = config.hash;
+
+        socket.on('connect', function() {
+            socket.emit("ch-subscribe-hash", hook);
+            node.status({ fill: 'green', shape: 'dot', text: 'connected' });
+        });
+
+        socket.on('disconnect', function () {
+            node.status({ fill: 'red', shape: 'ring', text: 'disconnected' });
+        });
+
+        socket.on('connect_error', function(err) {
+            node.status({ fill: 'red', shape: 'ring', text: 'error' });
+        });
+
+        socket.on("ch-get", function (data) {
+            node.send({ topic: hook, payload: data, method: "GET"});
+        })
+
+        socket.on("ch-post", function (data) {
+            node.send({ topic: hook, payload: data, method: "POST" });
+        })
+
+        node.on('close', function (done) {
+            socket.disconnect();
+            node.status({});
+            done();
+        });
+    }
+    RED.nodes.registerType('cloudhook-hash', CloudhookHash);
 
     
 };
